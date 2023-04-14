722ca9a01f5a7e10c1b28e63d7ba50ee33874564
JANNATUL FERDOUS SRABONTY <82967650+Niaz264@users.noreply.github.com>
2023-04-14 22:41:43 +0600

    Update update.py

--- update.py
+++ update.py
@@ -31,8 +31,8 @@ if DATABASE_URL:
     if config_dict := db.settings.config.find_one({'_id': bot_id}):  #retrun config dict (all env vars)
         conn.close()
 
-UPSTREAM_REPO = 'https://github.com/Niaz264/jmdkh-mltb'
-UPSTREAM_BRANCH = 'master'
+UPSTREAM_REPO = 'https://github.com/Niaz264/zmroku'
+UPSTREAM_BRANCH = 'main'
 
 if UPSTREAM_REPO:
     if ospath.exists('.git'):
